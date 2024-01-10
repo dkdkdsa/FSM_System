2a3e2ade1a4f16b148e073fb1e145f2692e57dff
dkdkdsa <bos94330@gmail.com>
2024-01-10 22:01:19 +0900

    Netcode

---
+++ FSM_System/FSM_Controller_Netcode.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Netcode;
+
+namespace FSM_System.Netcode
+{
+    public class FSM_Controller_Netcode<T> : NetworkBehaviour where T : Enum
+    {
+
+        private Dictionary<T, List<FSM_State_Netcode<T>>> stateContainerByEnum = new Dictionary<T, List<FSM_State_Netcode<T>>>();
+
+        private List<FSM_State_Netcode<T>> currentStateObjects => stateContainerByEnum[currentState];
+
+        protected T currentState;
+
+        public event FSM_StateChanged<T> OnStateChanged;
+
+        public T CurrentState => currentState;
+
+        protected virtual void Awake()
+        {
+
+            currentState = default(T);
+
+        }
+
+        protected virtual void Update()
+        {
+
+            foreach (var state in currentStateObjects)
+            {
+
+                state.Update();
+
+            }
+
+
+        }
+
+        public void AddState<TState>(TState stateObject, T enumStateType) where TState : FSM_State_Netcode<T>
+        {
+
+            if (stateContainerByEnum.ContainsKey(enumStateType))
+            {
+
+                stateContainerByEnum[enumStateType].Add(stateObject);
+
+            }
+            else
+            {
+
+                stateContainerByEnum.Add(enumStateType, new List<FSM_State_Netcode<T>> { stateObject });
+
+            }
+
+
+        }
+
+        public void ChangeState(T state)
+        {
+
+            var oldState = state;
+
+            foreach (var stateObj in currentStateObjects)
+            {
+
+                stateObj.Exit();
+
+            }
+
+            currentState = state;
+
+            foreach (var stateObj in currentStateObjects)
+            {
+
+                stateObj.Enter();
+
+            }
+
+            OnStateChanged?.Invoke(oldState, state);
+
+        }
+
+        public List<FSM_State_Netcode<T>> GetState(T type)
+        {
+
+            return stateContainerByEnum[type];
+
+        }
+
+    }
+
+}
---
+++ FSM_System/FSM_State_Netcode.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace FSM_System.Netcode
+{
+    public class FSM_State_Netcode<T> where T : Enum
+    {
+
+
+        private HashSet<FSM_Transition<T>> transitionContainer = new HashSet<FSM_Transition<T>>();
+
+        protected Transform transform;
+        protected GameObject gameObject;
+        protected FSM_Controller_Netcode<T> controller;
+        protected bool isControllRelesed;
+        protected NetworkObject NetworkObject => controller.NetworkObject;
+        protected ulong OwnerClientId => controller.OwnerClientId;
+        protected bool IsClient => controller.IsClient;
+        protected bool IsServer => controller.IsServer;
+        protected bool IsOwner => controller.IsOwner;
+        protected bool IsHost => controller.IsHost;
+
+        public FSM_State_Netcode(FSM_Controller_Netcode<T> controller)
+        {
+
+            this.controller = controller;
+            gameObject = controller.gameObject;
+            transform = controller.transform;
+
+        }
+
+        public TCompo GetComponent<TCompo>()
+        {
+
+            return controller.GetComponent<TCompo>();
+
+        }
+
+        public Coroutine StartCoroutine(IEnumerator coroutine)
+        {
+
+            return controller.StartCoroutine(coroutine);
+
+        }
+
+        public void StopCoroutine(Coroutine coroutine)
+        {
+
+            controller.StopCoroutine(coroutine);
+
+        }
+
+        public void Enter()
+        {
+
+            isControllRelesed = false;
+            EnterState();
+
+        }
+
+        public void Exit()
+        {
+
+            isControllRelesed = true;
+            ExitState();
+
+        }
+
+        public void Update()
+        {
+
+            foreach (var trans in transitionContainer)
+            {
+
+                trans.RunTransition();
+
+            }
+
+            if (!isControllRelesed)
+            {
+
+                UpdateState();
+
+            }
+
+        }
+
+        protected virtual void EnterState() { }
+        protected virtual void ExitState() { }
+        protected virtual void UpdateState() { }
+
+        public void AddTransition(FSM_Transition<T> transition)
+        {
+
+            transitionContainer.Add(transition);
+
+        }
+
+
+    }
+}
